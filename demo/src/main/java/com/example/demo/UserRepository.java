e1e69413bf236958f53508a1ba1f3b4e77159c7d
GuJauk <wkdnr1254@gmail.com>
2024-08-08 18:04:51 +0900

    로직 업로드

--- demo/src/main/java/com/example/demo/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface UserRepository extends JpaRepository<User, String> {
-    Optional<User> findById(String id); // 사용자 아이디로 검색
-    Optional<User> findByUniqueNumber(String uniqueNumber); // 고유번호로 검색
-}
